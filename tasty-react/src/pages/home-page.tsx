fb1c3b05a20d610dd324d46fc2d64fa989c4ad26
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2024-06-19 13:44:11 +0200

    react: recipe page

--- tasty-react/src/pages/home-page.tsx
+++ tasty-react/src/pages/home-page.tsx
@@ -10,7 +10,6 @@ import {
 import { RecipeType } from '@/types';
 import { useEffect, useState } from 'react';
 
-
 export default function HomePage() {
   const [recipes, setRecipes] = useState<RecipeType[]>([]);
   const [filterRecipes, setFilterRecipes] = useState<RecipeType[]>([]);
@@ -84,44 +83,43 @@ export default function HomePage() {
           <div className='grid grid-cols- md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-x-10 gap-y-20 xl:gap-y-32 xl:pt-32 pt-12 pb-40'>
             {(filterRecipes.length > 0 ? filterRecipes : recipes).map(
               (recipe: RecipeType, idx: number) => (
-                <Card
-                  key={`${recipe.name}-${idx}`}
-                  className='flex flex-col bg-orange-50 hover:scale-105 ease-in duration-200 xl:min-h-[600px] fancyGradient'
-                >
-                  <CardHeader>
-                    <img
-                      src={recipe.image}
-                      alt={recipe.name}
-                      width={500}
-                      height={500}
-                      className='bg-cover rounded-md shadow-xl'
-                    />
-                  </CardHeader>
-                  <CardContent>
-                    <CardTitle className='uppercase lg:text-3xl relative font-bold line-clamp-2'>
-                      {recipe.name}
-                    </CardTitle>
-                    <CardDescription>{recipe.cuisine}</CardDescription>
-                  </CardContent>
-                  <CardFooter className='flex items-start gap-2 lg:gap-12 lg:flex-row flex-col'>
-                    <div className='flex flex-col'>
-                      <p className='text-lg'>Serves</p>
-                      <p className='text-gray-800'>{recipe.servings}</p>
-                    </div>
-                    <div className='flex flex-col'>
-                      <p className='text-lg'>Prep Time</p>
-                      <p className='text-gray-800'>
-                        {recipe.prepTimeMinutes} MIN
-                      </p>
-                    </div>
-                    <div className='flex flex-col'>
-                      <p className='text-lg'>Cook Time</p>
-                      <p className='text-gray-800'>
-                        {recipe.cookTimeMinutes} MIN
-                      </p>
-                    </div>
-                  </CardFooter>
-                </Card>
+                <a href={`/recipes/${recipe.id}`} key={`${recipe.name}-${idx}`}>
+                  <Card className='flex flex-col bg-orange-50 hover:scale-105 ease-in duration-200 xl:min-h-[600px] fancyGradient'>
+                    <CardHeader>
+                      <img
+                        src={recipe.image}
+                        alt={recipe.name}
+                        width={500}
+                        height={500}
+                        className='bg-cover rounded-md shadow-xl'
+                      />
+                    </CardHeader>
+                    <CardContent>
+                      <CardTitle className='uppercase lg:text-3xl relative font-bold line-clamp-2'>
+                        {recipe.name}
+                      </CardTitle>
+                      <CardDescription>{recipe.cuisine}</CardDescription>
+                    </CardContent>
+                    <CardFooter className='flex items-start gap-2 lg:gap-12 lg:flex-row flex-col'>
+                      <div className='flex flex-col'>
+                        <p className='text-lg'>Serves</p>
+                        <p className='text-gray-800'>{recipe.servings}</p>
+                      </div>
+                      <div className='flex flex-col'>
+                        <p className='text-lg'>Prep Time</p>
+                        <p className='text-gray-800'>
+                          {recipe.prepTimeMinutes} MIN
+                        </p>
+                      </div>
+                      <div className='flex flex-col'>
+                        <p className='text-lg'>Cook Time</p>
+                        <p className='text-gray-800'>
+                          {recipe.cookTimeMinutes} MIN
+                        </p>
+                      </div>
+                    </CardFooter>
+                  </Card>
+                </a>
               )
             )}
           </div>
--- tasty-react/src/pages/recipe-page.tsx
+++ tasty-react/src/pages/recipe-page.tsx
@@ -1,5 +1,100 @@
+import { RecipeType } from '@/types';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 export default function RecipePage() {
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState<RecipeType | null>(null);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const response = await fetch(`https://dummyjson.com/recipes/${recipeId}`);
+      const data = await response.json();
+      setRecipe(data);
+    };
+
+    if (recipeId) {
+      fetchRecipe();
+    }
+  }, [recipeId]);
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+  console.log(recipe);
   return (
-    <div>RecipePage</div>
-  )
+    <div className='xl:px-48 px-10 pb-12 fancyGradient'>
+      <div className='grid xl:grid-cols-2 pb-10 xl:pb-20'>
+        <div className='pt-12 xl:pt-12'>
+          <Link to='/' className='text-2xl'>
+            <p>← Back to All Recipes</p>
+          </Link>
+          <h1 className='text-4xl lg:text-6xl my-8 uppercase'>{recipe.name}</h1>
+          <div className='grid grid-cols-3 gap-12'>
+            <div className='flex flex-col'>
+              <p className='text-xl lg:text-3xl uppercase'>🍽 Serves</p>
+              <p className='text-grey-800 text-2xl lg:text-3xl font-bold'>
+                {recipe.servings}
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-xl lg:text-3xl uppercase'>⌛ Prep Time</p>
+              <p className='text-grey-800 text-2xl lg:text-3xl font-bold'>
+                {recipe.prepTimeMinutes} MIN
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-xl lg:text-3xl uppercase'>🕒 Cook Time</p>
+              <p className='text-grey-800 text-2xl lg:text-3xl font-bold'>
+                {recipe.cookTimeMinutes} MIN
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-xl lg:text-3xl uppercase'>🍔 Cuisine</p>
+              <p className='text-grey-800 text-2xl lg:text-3xl font-bold'>
+                {recipe.cuisine}
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-xl lg:text-3xl uppercase'>🔥 Difficulty</p>
+              <p className='text-grey-800 text-2xl lg:text-3xl font-bold'>
+                {recipe.difficulty}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='py-4 mx-auto'>
+          <img
+            src={recipe.image}
+            width={400}
+            height={400}
+            alt={recipe.name}
+            className='w-96 h-96 lg:h-[600px] lg:w-[600px]'
+          />
+        </div>
+      </div>
+      <div className='grid xl:grid-cols-2'>
+        <div className='xl:px-12'>
+          <h2 className='uppercase text-5xl my-12'>Ingredients</h2>
+          <div className='flex flex-col'>
+            {recipe.ingredients.map((ingredient: string, idx: number) => (
+              <ul key={`${ingredient}-${idx}`} className='py-2 border-b border-gray-950'>
+                <li className='text-2xl'>{ingredient}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+        <div className='xl:px-12'>
+          <h2 className='uppercase text-5xl my-12'>Instructions</h2>
+          <div className='flex flex-col'>
+            {recipe.instructions.map((instruction: string, idx: number) => (
+              <ul key={`${instruction}-${idx}`} className='py-2 list-disc'>
+                <li className='text-2xl'>{instruction}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
